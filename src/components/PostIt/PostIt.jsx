0661746076062686c2fb274539cfd178901ba931
Rômulo Oliveira Torres <romulo.torres@agefis.fortaleza.ce.gov.br>
2019-06-28 14:29:08 -0300

    Ajustes no PostItForm

--- src/components/PostIt/PostIt.jsx
+++ src/components/PostIt/PostIt.jsx
@@ -1,28 +1,21 @@
-import React, {Component} from 'react';
-import './PostIt.css';
+import React from "react";
+import "./PostIt.css";
 
-export default class PostIt extends Component {
-
-    constructor(props) {
-        super(props);
-        this.props = props;
-        this.state = {
-            remetente: '',
-            destinatario: '',
-            dataCriacao: '',
-            cor: '',
-            lido: false
-        }
-    }
-
-    render() {
-        return (
-            <div className='borda'>
-                <p>Remetente: <span>{this.state.remetente}</span></p>
-                <p>Destinatário: <span>{this.state.remetente}</span></p>
-                <p>Data de criação: <span>{this.state.remetente}</span></p>
-                <p>Lido?: <span>{this.state.remetente ? 'SIM' : 'NÃO'}</span></p>
-            </div>
-        )
-    }
+export default function(props) {
+  return (
+    <div className="borda">
+      <p>
+        Remetente: <span>{props.remetente}</span>
+      </p>
+      <p>
+        Destinatário: <span>{props.destinatario}</span>
+      </p>
+      <p>
+        Data de criação: <span>{props.dataCriacao}</span>
+      </p>
+      <p>
+        Lido?: <span>{props.lido}</span>
+      </p>
+    </div>
+  );
 }
--- src/components/PostItForm/PostItForm.jsx
+++ src/components/PostItForm/PostItForm.jsx
@@ -13,49 +13,84 @@ export default class PostItForm extends Component {
       lido: false
     };
 
-    this.salvar.bind(this);
+    this.salvar = this.salvar.bind(this);
   }
 
   salvar() {
-    console.log("Teste");
+    console.log(this.state);
   }
 
   render() {
     return (
       <div className="postItForm">
-        <label for="remetente">Remetente</label>
-        <input
-          class="form-control"
-          type="text"
-          id="remetente"
-          name="remetente"
-        />
+        <div className="row">
+          <div className="col-md-2">
+            <input
+              placeholder="Remetente"
+              className="form-control"
+              type="text"
+              id="remetente"
+              name="remetente"
+              onChange={remetente =>
+                this.setState({ remetente: remetente.target.value })
+              }
+            />
+          </div>
 
-        <label for="destinatario">Destinatário</label>
-        <input
-          class="form-control"
-          type="text"
-          id="destinatario"
-          name="destinatario"
-        />
+          <div className="col-md-2">
+            <input
+              placeholder="Destinatario"
+              className="form-control"
+              type="text"
+              id="destinatario"
+              name="destinatario"
+              onChange={destinatario =>
+                this.setState({ destinatario: destinatario.target.value })
+              }
+            />
+          </div>
 
-        <label for="dataCriacao">Data de criação:</label>
-        <input
-          class="form-control"
-          type="date"
-          id="dataCriacao"
-          name="dataCriacao"
-        />
+          <div className="col-md-2">
+            <input
+              className="form-control"
+              type="date"
+              id="dataCriacao"
+              name="dataCriacao"
+              onChange={dataCriacao =>
+                this.setState({ dataCriacao: dataCriacao.target.value })
+              }
+            />
+          </div>
 
-        <label for="cor">Escolha uma cor:</label>
-        <input class="form-control" type="color" id="cor" name="cor" />
+          <div className="col-md-1">
+            <input
+              className="form-control"
+              type="color"
+              id="cor"
+              name="cor"
+              onChange={cor => this.setState({ cor: cor.target.value })}
+            />
+          </div>
 
-        <label for="lembrete">Lembrete:</label>
-        <input class="form-control" type="text" id="lembrete" name="lembrete" />
+          <div className="col-md-3">
+            <input
+              placeholder="Lembrete..."
+              className="form-control"
+              type="text"
+              id="lembrete"
+              name="lembrete"
+              onChange={lembrete =>
+                this.setState({ lembrete: lembrete.target.value })
+              }
+            />
+          </div>
 
-        <button class="btn btn-primary" onClick={this.salvar}>
-          Salvar
-        </button>
+          <div className="col-md-2">
+            <button className="btn btn-primary" onClick={this.salvar}>
+              Salvar
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
